969267df99cec70142485c6d4dbe5d9b8a077f23
gregott91 <gregoryott2345@gmail.com>
2021-01-17 15:08:09 -0600

    initial templating

--- main.go
+++ main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"html/template"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
-	fmt.Println("hello worlds")
+	example()
+}
+
+func example() {
+	const tpl = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>{{.Title}}</title>
+	</head>
+	<body>
+		{{range .Items}}<div>{{ . }}</div>{{else}}<div><strong>no rows</strong></div>{{end}}
+	</body>
+</html>`
+
+	f, err := os.Create("D:\\local\\temp\\somefile.html")
+	check(err)
+
+	defer f.Close()
+
+	t, err := template.New("webpage").Parse(tpl)
+	check(err)
+
+	data := struct {
+		Title string
+		Items []string
+	}{
+		Title: "My page",
+		Items: []string{
+			"My photos",
+			"My blog",
+		},
+	}
+
+	w := bufio.NewWriter(f)
+	err = t.Execute(w, data)
+	check(err)
+
+	noItems := struct {
+		Title string
+		Items []string
+	}{
+		Title: "My another page",
+		Items: []string{},
+	}
+
+	err = t.Execute(w, noItems)
+	check(err)
+
+	w.Flush()
 }
